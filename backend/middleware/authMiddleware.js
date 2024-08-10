406c406931f31f3a005e0dabe3e540adb1ed1b17
Ivan Caballero <2122100584@soy.utj.edu.mx>
2024-08-10 04:32:26 -0600

    frontend login registro

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,17 +1,20 @@
-const jwt = require('jsonwebtoken');
+// authMiddleware.js
 
 module.exports = (req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '');
-
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    console.log('Token recibido:', token);
+  
     if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
+      return res.status(401).json({ msg: 'No token, authorization denied' });
     }
-
+  
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-        next();
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+      console.error('Error al verificar el token:', err.message);
+      res.status(401).json({ msg: 'Token is not valid' });
     }
-};
+  };
+  
